6b72b6f19fe6088646a2b8b3fd774402d878d86d
Srinitha-j <jsrinitha05@gmail.com>
2026-02-16 20:41:04 +0530

    Add files via upload

---
+++ lab3.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+/*
+using namespace std;
+
+int main(){
+
+    int num,sum=0;
+    do{
+        sum += num;
+        cout<<"enter the number:";
+        cin >> num;
+
+
+
+    }while(num>=0);
+    cout<<"sum :"<<sum;
+    return 0;
+}
+*/
+/*
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+    float a, b;
+
+    do {
+        cout << "\n----- MENU -----\n";
+        cout << "1. Add\n";
+        cout << "2. Subtract\n";
+        cout << "3. Multiply\n";
+        cout << "4. Divide\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice >= 1 && choice <= 4) {
+            cout << "Enter two numbers: ";
+            cin >> a >> b;
+        }
+
+        if (choice == 1) {
+            cout << "Result = " << a + b << endl;
+        }
+        else if (choice == 2) {
+            cout << "Result = " << a - b << endl;
+        }
+        else if (choice == 3) {
+            cout << "Result = " << a * b << endl;
+        }
+        else if (choice == 4) {
+            if (b != 0)
+                cout << "Result = " << a / b << endl;
+            else
+                cout << "Error: Cannot divide by zero!" << endl;
+        }
+        else if (choice == 5) {
+            cout << "Exiting program..." << endl;
+        }
+        else {
+            cout << "Invalid choice! Try again.\n";
+        }
+
+    } while (choice != 5);
+
+    return 0;
+}
+*/
+
